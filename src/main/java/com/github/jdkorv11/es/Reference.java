77aa8016208c0d07ca05250e0310a8df4b464894
jkorve1 <Joshua.Korver@ampf.com>
2015-11-16 12:25:56 -0600

    setup dev environment

---
+++ src/main/java/com/github/jdkorv11/es/Reference.java
@@ -0,0 +1,10 @@
+package com.github.jdkorv11.es;
+
+public class Reference {
+	public static final String MODID = "es";
+    public static final String NAME = "Experimental Stuff";
+    public static final String VERSION = "1.0.0";
+    public static final String COMMON_PROXY = "com.github.jdkorv11.es.proxies.CommonProxy";
+    public static final String CLIENT_PROXY = "com.github.jdkorv11.es.proxies.ClientProxy";
+
+}
